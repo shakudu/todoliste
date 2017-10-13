1cded0a91747d38f7f467e8c91da925e97731829
Gard Marius Guldhav <gard@gitt.no>
2017-10-13 15:42:35 +0200

    Diverse GUI-fix

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,6 +1,6 @@
 function displayAlert(type,header,message) {
     var divId = "alerts";
-    var html = '<div class="alert alert-' + type + ' alert-dismissable fade show" role="alert">' +
+    var html = '<div class="alert alert-' + type + ' alert-dismissable fade in" role="alert">' +
         '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
         '<strong>' + header + '!</strong> ' + message +'</div>';
     $("#" + divId).html(html);
